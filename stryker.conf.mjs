ab76532c944fa63cfc24e998ea1e9539ff032651
OrSasson1407 <orsasson1407@gmail.com>
2026-06-08 21:55:30 +0300

    fix mjutant bug

--- stryker.conf.mjs
+++ stryker.conf.mjs
@@ -1,13 +1,18 @@
-export default {
+﻿export default {
   testRunner: "vitest",
   plugins: ["@stryker-mutator/vitest-runner"],
+  vitest: {
+    configFile: "vitest.workspace.ts",
+  },
   reporters: ["html", "clear-text", "progress"],
   coverageAnalysis: "perTest",
   mutate: [
     "packages/core/src/**/*.ts",
     "!packages/core/src/**/*.test.ts",
+    "!packages/core/src/test-setup.ts",
     "packages/extension/src/**/*.ts",
     "!packages/extension/src/**/*.test.ts",
+    "!packages/extension/src/test-setup.ts",
   ],
   thresholds: {
     high: 90,
